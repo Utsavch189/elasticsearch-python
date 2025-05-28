7b6d831906c8c6e41af9a5561d14edaa4b1d67f7
Utsavch189 <utsavpokemon9000chatterjee@gmail.com>
2025-05-28 15:28:55 +0530

    update

---
+++ screener/main.py
@@ -0,0 +1,22 @@
+from flask import Flask,request
+from elastic import es
+from query_builder import build_query_from_expression
+
+app = Flask(__name__)
+
+@app.post("/screener")
+def screener():
+    query = request.json.get('query')
+    expression = build_query_from_expression(
+        expr=query
+    )
+    response = es.search(index="financial_reports", body=expression,size=2000)
+    
+    res = []
+    
+    for hit in response['hits']['hits']:
+        res.append(hit['_source'])
+    return res
+
+if __name__ == "__main__":
+    app.run(port=4000,debug=True)
--- screener/query_builder.py
+++ screener/query_builder.py
@@ -21,44 +21,69 @@ def parse_comparison(cond):
                 return {"range": {field: {op_map[op]: value}}}
     raise ValueError(f"Invalid condition: {cond}")
 
+def get_fields(cond):
+    op_map = {
+        '>=': 'gte',
+        '<=': 'lte',
+        '>': 'gt',
+        '<': 'lt',
+        '=': 'eq',
+        '==': 'eq'
+    }
+    
+    for op in ['>=', '<=', '>', '<', '==', '=']:
+        if op in cond:
+            print("hiii ==> ",op)
+            field, value = cond.split(op)
+            field = field.strip().replace(" ", "_")
+            return field
+
 def build_query_from_expression(expr):
     expr = expr.lower().replace(" and ", " AND ").replace(" or ", " OR ")
 
     or_parts = [part.strip() for part in expr.split("OR")]
 
     should_clauses = []
+    source = ["fincode","year_end_x"]
+
     for part in or_parts:
         if "AND" in part:
             and_conditions = [c.strip() for c in part.split("AND")]
             must_clauses = [parse_comparison(c) for c in and_conditions]
             should_clauses.append({"bool": {"must": must_clauses}})
+            for f in and_conditions:
+                source.append(get_fields(f))
         else:
             should_clauses.append(parse_comparison(part))
+            source.append(get_fields(part))
 
     return {
+        "_source": source,
         "query": {
             "bool": {
-                # "filter": [  # mandatory filter clause
-                #     {"term": {"year_end_x": 202303}}
-                # ],
+                "filter": [ 
+                    {"term": {"year_end_x": 202303}}
+                ],
                 "should": should_clauses,
                 "minimum_should_match": 1
             }
         }
     }
 
-start = time.time()
-# Example usage
-expression = "net_profit_growth_percentage_cons_2year > 10% AND net_margin_growth_percentage_cons_3year > 20%"
-query = build_query_from_expression(expression)
-print(expression)
-# Elasticsearch call
-response = es.search(index="financial_reports", body=query,size=300)
+if __name__=="__main__":
+    start = time.time()
+    # Example usage
+    expression = "net_profit_growth_percentage_cons_2year > 10% OR net_margin_growth_percentage_cons_3year > 20%"
+    query = build_query_from_expression(expression)
+    # print(expression)
 
-res = []
-# Print results
-for hit in response['hits']['hits']:
-    res.append(hit['_source'])
+    response = es.search(index="financial_reports", body=query,size=2000)
 
-print("res ====> ",len(res))
-print("Total time in seconds ===>  ",time.time()-start)
+    res = []
+
+    for hit in response['hits']['hits']:
+        res.append(hit['_source'])
+
+    print("res length ====> ",len(res))
+    print("res ====> ",res)
+    print("Total time in seconds ===>  ",time.time()-start)
